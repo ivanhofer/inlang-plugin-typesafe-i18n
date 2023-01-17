b56a958e92a4a62f47bafb7e77ea8710e53777bf
Ivan Hofer <ivan.hofer@outlook.com>
2023-01-17 09:30:49 +0100

    read config from disk

--- src/index.ts
+++ src/index.ts
@@ -1,30 +1,41 @@
 import type { Config, EnvironmentFunctions } from "@inlang/core/config";
 import type * as ast from "@inlang/core/ast";
+import { getConfig } from 'typesafe-i18n/config'
 
 // issues:
 //  - real typescript compilation does not work
-//  - $fs.readFile does not support nested files
 //  - $import can't be used to import a base64 encoded string
 
 export type PluginConfig = {};
 
 type ReadResourcesArgs = Parameters<Config["readResources"]>[0] & EnvironmentFunctions & { pluginConfig: PluginConfig }
 
+const resolve = (...parts: string[]): string => parts.map(p => {
+  while (p.startsWith('/')) {
+    p = p.substring(1)
+  }
+  while (p.endsWith('/')) {
+    p = p.substring(0, p.length - 1)
+  }
+  return p
+}).join('/')
+
 export async function readResources(
   { config, $fs, $import }: ReadResourcesArgs
 ): ReturnType<Config["readResources"]> {
-  const result: ast.Resource[] = [];
+  const typesafeI18nConfig = await getConfig($fs)
 
+  const result: ast.Resource[] = [];
   for (const language of config.languages) {
-    const dictionary = await getDictionaryForLocale($fs, $import, language)
+    const dictionary = await getDictionaryForLocale($fs, $import, typesafeI18nConfig.outputPath, language)
     result.push(parseResource(dictionary, language));
   }
 
   return result
 }
 
-const getDictionaryForLocale = async ($fs: EnvironmentFunctions['$fs'], $import: EnvironmentFunctions['$import'], locale: string) => {
-  const baseDictionary = (await $fs.readFile(`src/i18n/${locale}/index.ts`, 'utf-8')).toString()
+const getDictionaryForLocale = async ($fs: EnvironmentFunctions['$fs'], $import: EnvironmentFunctions['$import'], outputPath: string, locale: string) => {
+  const baseDictionary = (await $fs.readFile(resolve(outputPath, `${locale}/index.ts`), 'utf-8')).toString()
   const withoutImports = baseDictionary.split('\n').filter(line => !line.trim().startsWith('import ')).join('\n')
   const withoutTypes = withoutImports.replace(/:.*=/g, ' =')
 
@@ -70,20 +81,23 @@ const parseMessage = (id: string, value: string): ast.Message => {
 type WriteResourcesArgs = Parameters<Config["writeResources"]>[0] & EnvironmentFunctions & { pluginConfig: PluginConfig }
 
 export async function writeResources(
-  args: WriteResourcesArgs
+  { $fs, config, resources }: WriteResourcesArgs
 ): ReturnType<Config["writeResources"]> {
-  for (const resource of args.resources) {
+  const typesafeI18nConfig = await getConfig($fs)
+
+  for (const resource of resources) {
     const locale = resource.languageTag.language
     const dictionary = serializeResource(resource)
 
-    const type = locale === args.config.referenceLanguage ? 'BaseTranslation' : 'Translation'
-    const template = `import type { ${type} } from './src/i18n/i18n-types'
+    const type = locale === config.referenceLanguage ? 'BaseTranslation' : 'Translation'
+    // TODO: path could be wrong if esmImports=true
+    const template = `import type { ${type} } from './${resolve(typesafeI18nConfig.outputPath, typesafeI18nConfig.typesFileName)}'
 
 const ${locale}: ${type} = ${dictionary}
 
 export default ${locale}`
 
-    await args.$fs.writeFile(`${locale}.ts`, template);
+    await $fs.writeFile(`${locale}.ts`, template);
   }
 }
 
