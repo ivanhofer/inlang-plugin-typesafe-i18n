41a978f8b16bd3617619b60030fb3ff3bf798f90
Ivan Hofer <ivan.hofer@outlook.com>
2023-01-17 11:28:15 +0100

    get locale information directly from `typesafe-i18n`

--- example/inlang.config.js
+++ example/inlang.config.js
@@ -1,3 +1,5 @@
+import { getLocaleInformation } from 'typesafe-i18n/config'
+
 /**
  * @type {import("@inlang/core/config").InitializeConfig}
  */
@@ -5,9 +7,11 @@ export async function initializeConfig(env) {
   // importing plugin from local file for testing purposes
   const plugin = await env.$import("../dist/index.js");
 
+  const { base, locales } = await getLocaleInformation(env.$fs)
+
   return {
-    referenceLanguage: "en",
-    languages: ["en", "de"],
+    referenceLanguage: base,
+    languages: locales,
     readResources: (args) =>
       plugin.readResources({ ...args, ...env }),
     writeResources: (args) =>
