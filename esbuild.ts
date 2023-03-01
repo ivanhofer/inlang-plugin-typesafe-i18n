1f781ed362c086b3465db10ae1edd4b40f067f23
Ivan Hofer <ivan.hofer@outlook.com>
2023-03-01 07:35:09 +0100

    bundle `@inlang/core` again

--- esbuild.ts
+++ esbuild.ts
@@ -9,7 +9,6 @@ const ctx = await context({
 	format: 'esm',
 	platform: 'browser',
 	target: 'es2020',
-	external: ['@inlang/core', 'typesafe-i18n'],
 	plugins: [
 		// by default node polyfills are included
 		// as a lot of npm packages that deal with files
