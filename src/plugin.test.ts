72c27a6463c2d1f95bed5fc882021fa1c341236c
Samuel Stroschein <35429197+samuelstroschein@users.noreply.github.com>
2023-05-01 12:14:19 +0200

    update to @inlang/core 0.7.8

--- src/plugin.test.ts
+++ src/plugin.test.ts
@@ -1,5 +1,5 @@
 import { expect, test } from "vitest";
-import { mockEnvironment, validateConfig } from "@inlang/core/test";
+import { mockEnvironment, testConfig } from "@inlang/core/test";
 import { setupConfig } from "@inlang/core/config";
 import fs from "node:fs/promises";
 
@@ -15,7 +15,7 @@ test("inlang's config validation should pass", async () => {
 
   const config = await setupConfig({ module, env });
 
-  const [isOk, error] = await validateConfig({ config });
+  const [isOk, error] = await testConfig({ config });
   if (error) {
     throw error;
   }
