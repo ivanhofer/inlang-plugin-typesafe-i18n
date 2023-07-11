d74432f69980d2471137a2022e40ff9838c0519c
Ivan Hofer <ivan.hofer@outlok.com>
2023-07-11 15:28:59 +0200

    fix detection of type when pasring dictionary

--- src/readResources.ts
+++ src/readResources.ts
@@ -36,20 +36,17 @@ const getDictionaryForLocale = async (
 	locale: string
 ) => {
 	// TODO: create a better, less hacky version
-	const baseDictionary = (
-		await $fs.readFile(resolve(outputPath, `${locale}/index.ts`), {
-			encoding: "utf-8",
-		})
-	).toString()
+	const baseDictionary = await $fs.readFile(resolve(outputPath, `${locale}/index.ts`), { encoding: "utf-8" })
+
 	const withoutImports = baseDictionary
 		.split("\n")
 		.filter((line) => !line.trim().startsWith("import "))
 		.join("\n")
-	const withoutTypes = withoutImports.replace(/:.*=/g, " =")
+	const withoutTypes = withoutImports
+		.replace(/const\s\w[^\s]+\s*(:\s*\w[^\s]+\s*)=/g, (match, type) => match.replace(type, ""))
 	const withoutSatisfies = withoutTypes.replace(/ satisfies.*\/n/g, "\n")
 
-	const moduleWithMimeType =
-		"data:application/javascript," + encodeURIComponent(withoutSatisfies)
+	const moduleWithMimeType = "data:application/javascript," + encodeURIComponent(withoutSatisfies)
 	return (await import(/* @vite-ignore */ moduleWithMimeType)).default
 }
 
